5db1d24377a4889d3e20eff4dc7ccbd116e55862
Kelly Bolding <kbolding@princeton.edu>
2019-04-29 10:38:26 -0400

    Added new version of bash script.
    This version creates a new CSV file (using * as the delimiter) that includes the language code returned from Google.

---
+++ log-languages-in-csv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# loops over csv file in bash and posts each line to the google translate API
+# creates new csv file with returned language code inserted as an additional field (separator is *)
+# note: see http://www.unixguide.net/unix/bash/E4.shtml for why you can't just create a new variable by piping the curl output to read
+
+#replace paths to match location of local files
+
+export GOOGLE_APPLICATION_CREDENTIALS="/Users/rbscadmin/Desktop/scripting-test-files/private-key.json"
+
+while IFS=* read f1 f2 f3
+do
+langcode=$(curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) --data "{'q': '$f3'}" "https://translation.googleapis.com/language/translate/v2/detect" | jq -r ".data.detections[0][0].language")
+echo "$f1*$f2*$f3*$langcode" >> "/Users/rbscadmin/Desktop/log.csv"
+done < "/Users/rbscadmin/Desktop/scripting-test-files/output.csv"
